94b9c59b723420a1a4d95e80c99238764a91ead5
= <=>
2022-05-07 13:58:45 +0530

    Done with Editor design

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,7 +1,25 @@
-import React from 'react';
-
+import CodeMirror from 'codemirror';
+import React, { useEffect } from 'react';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/dracula.css';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/closebrackets';
 const Editor = () => {
-  return <div></div>;
+  useEffect(() => {
+    async function init() {
+      CodeMirror.fromTextArea(document.getElementById('realtimeEditor'), {
+        mode: { name: 'javascript', json: true },
+        theme: 'dracula',
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        lineNumbers: true,
+      });
+    }
+    init();
+  }, []);
+
+  return <textarea id='realtimeEditor'></textarea>;
 };
 
 export default Editor;
