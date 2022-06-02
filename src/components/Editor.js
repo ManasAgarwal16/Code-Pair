360a403aafa0de20514981732276cd92c8623db4
= <=>
2022-06-02 23:03:40 +0530

    Automatic Sync Final Commit

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -25,7 +25,7 @@ const Editor = ({ socketRef, roomId, onCodeChange }) => {
       editorRef.current.on('change', (instance, changes) => {
         const { origin } = changes;
         const code = instance.getValue();
-        // onCodeChange(code);
+        onCodeChange(code);
         if (origin !== 'setValue') {
           // console.log('working', code);
           socketRef.current.emit(ACTIONS.CODE_CHANGE, {
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -108,7 +108,13 @@ const EditorPage = () => {
         </button>
       </div>
       <div className='editorWrap'>
-        <Editor socketRef={socketRef} roomId={roomId} />
+        <Editor
+          socketRef={socketRef}
+          roomId={roomId}
+          onCodeChange={(code) => {
+            codeRef.current = code;
+          }}
+        />
       </div>
     </div>
   );
